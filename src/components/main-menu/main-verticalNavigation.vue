f9f4aa2af1fe588564f36b97712998ce9664b0cf
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2023-10-30 11:15:42 +0100

    fix MainVerticalNav link

--- src/components/main-menu/main-verticalNavigation.vue
+++ src/components/main-menu/main-verticalNavigation.vue
@@ -4,9 +4,9 @@
     <nav class="bg-gray-800 text-white">
       <ul class="listOfModel">
         <div class="allModel">All Model</div>
-        <li v-for="link in links" :key="link.id" :title="link.model" class="listModel">
-          <router-link @click="handleLinkClick(link.model)" :to="link.to" class="linkModel">
-            <div>{{ link.text }}</div>
+        <li v-for="link in data" :key="link.id" :title="link.model" class="listModel">
+          <router-link @click="handleLinkClick()" :to="{ path: '/operation-menu/', query: { model: link }}" class="linkModel">
+            <div>{{ link }}</div>
           </router-link>
         </li>
       </ul>
@@ -15,29 +15,9 @@
 </template>
 
 <script setup>
-import { ref, defineProps, defineEmits } from 'vue';
+import { ref, defineProps } from 'vue';
+const { data } = await useFetch('/api/models')
 
-const links = [
-  { id: 1, to: '/operation-menu', text: 'Building', model: 'Building' },
-  { id: 2, to: '/operation-menu', text: 'Floor', model: 'Floor' },
-  { id: 3, to: '/operation-menu', text: 'Indication', model: 'Indication' },
-  { id: 4, to: '/operation-menu', text: 'Legend', model: 'Legend' },
-  { id: 5, to: '/operation-menu', text: 'Route', model: 'Route' },
-  { id: 6, to: '/operation-menu', text: 'Sensor', model: 'Sensor' },
-  { id: 7, to: '/operation-menu', text: 'SensorAggregatedValue', model: 'SensorAggregatedValue' },
-  { id: 8, to: '/operation-menu', text: 'SensorType', model: 'SensorType' },
-  { id: 9, to: '/operation-menu', text: 'SensorValue', model: 'SensorValue' },
-  { id: 10, to: '/operation-menu', text: 'SensorValueArchive', model: 'SensorValueArchive' },
-  { id: 11, to: '/operation-menu', text: 'Space', model: 'Space' },
-  { id: 12, to: '/operation-menu', text: 'Unit', model: 'Unit' },
-];
-
-const props = defineProps(['model']);
-const emits = defineEmits(['linkClicked']);
-
-const handleLinkClick = (model) => {
-  emits('linkClicked', model);
-};
 </script>
 
 <style scoped>
@@ -73,6 +53,7 @@ ul.listOfModel {
   margin: 0.5em 0em 0.5em 0em;
   background-size: 2em;
   line-height: 2em;
+  text-transform: capitalize;
 }
 
 .listModel:hover {
