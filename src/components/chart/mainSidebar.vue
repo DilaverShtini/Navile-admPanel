95e17ee6286b034231b5d923d7a9251eb566d0a1
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2024-01-25 11:37:15 +0100

    add a first implementation of sidebar interaction

--- src/components/chart/mainSidebar.vue
+++ src/components/chart/mainSidebar.vue
@@ -5,13 +5,18 @@ import "~/assets/css/component.css"
 const { data } = useNuxtData('charts') as {data: any};
 const charts = ['BarChart', 'PolarAreaChart', 'PieChart'];
 
+const showData = async (dataName: any) => {
+  const { } = await useAsyncData('dataToShow', () => dataName) as {data: any};
+
+}
+
 </script>
 
 <template>
   <nav class="bg-gray-800 text-white">
     GRAFICI
     <ul class="listOfChart">
-      <li v-for="link in charts" class="listChart">
+      <li v-for="link in charts" @click="showData(link)" class="listChart">
         <div class="row">
           <div class="chartName">
               {{ link }}
@@ -21,10 +26,20 @@ const charts = ['BarChart', 'PolarAreaChart', 'PieChart'];
     </ul>
     DATI
     <ul class="listOfData">
-      <li v-for="link in data" :key="link.id" :title="link.type" class="listData">
+      <li v-for="link in data" @click="showData(link.type)" :key="link.id" :title="link.type" class="listData">
         <div class="row">
           <div class="dataName">
             {{ link.type }}
+          </div>
+        </div>
+      </li>
+    </ul>
+    TUTTI
+    <ul class="listOfData">
+      <li @click="showData('All')" class="listData">
+        <div class="row">
+          <div class="dataName">
+            Visualizza tutti i dati
           </div>
         </div>
       </li>
--- src/pages/visualization-page.vue
+++ src/pages/visualization-page.vue
@@ -5,8 +5,8 @@
     import { MainSidebar } from '../utils';
     import "~/assets/css/main.css"
 
-    const { data } = await useAsyncData('charts', () => $fetch('/api/visualization/charts')) as {data: any};
-
+    const { } = await useAsyncData('charts', () => $fetch('/api/visualization/charts')) as {data: any};
+    
     export default {
         name: 'App',
         components: { BarChart, PolarAreaChart, PieChart, MainSidebar},
@@ -15,7 +15,14 @@
                 dataBarAnalysis: ['URL', 'Road'],
                 dataPolarAreaAnalysis: ['Indietro'],
                 dataPieAnalysis: ['Sidebar | Mappa'],
+                chartToDiplay: ref('All'),
             };
+        },
+        methods: {
+            updateView() {
+                const { data } = useNuxtData('dataToShow') as { data: any }
+                this.chartToDiplay = data.value
+            },
         },
     }
 </script>
@@ -24,11 +31,26 @@
     <div class="container">
         <div class="menu border">
             <div class="title"> Dati da visualizzare </div>
-            <MainSidebar />
+            <MainSidebar @click="updateView()"/>
         </div>
         <div class="form">
             <div class="form-container">
-                <BarChart :dataProp="dataBarAnalysis" />
+                <div v-if="chartToDiplay == 'BarChart'">
+                    <BarChart :dataProp="dataBarAnalysis" />
+                </div>
+                <div>
+                    <div v-if="chartToDiplay == 'PolarAreaChart'">
+                        <PolarAreaChart :dataProp="dataPolarAreaAnalysis"/>
+                    </div>
+                    <div v-if="chartToDiplay == 'PieChart'">
+                        <PieChart :dataProp="dataPieAnalysis"/>
+                    </div>
+                </div>
+            </div>
+            <div class="form-container" v-if="chartToDiplay == 'All'">
+                <div>
+                    <BarChart :dataProp="dataBarAnalysis" />
+                </div>
                 <div>
                     <div>
                         <PolarAreaChart :dataProp="dataPolarAreaAnalysis"/>
@@ -38,6 +60,32 @@
                     </div>
                 </div>
             </div>
+            <div class="form-container" v-if="chartToDiplay == 'URL'">
+                <div>
+                    <BarChart :dataProp="['URL']" />
+                </div>
+            </div>
+            <div class="form-container" v-if="chartToDiplay == 'Road'">
+                <div>
+                    <BarChart :dataProp="['Road']" />
+                </div>
+            </div>
+            <div class="form-container" v-if="chartToDiplay == 'Button'">
+                <div>
+                    <PolarAreaChart :dataProp="['Button']" />
+                </div>
+            </div>
+            <div class="form-container" v-if="chartToDiplay == 'Sidebar'">
+                <div>
+                    <PieChart :dataProp="['Sidebar | Mappa']" />
+                </div>
+            </div>
+            <div class="form-container" v-if="chartToDiplay == 'Mappa'">
+                <div>
+                    <PieChart :dataProp="['Sidebar | Mappa']" />
+                </div>
+            </div>
+            
         </div>
     </div>
 </template>
